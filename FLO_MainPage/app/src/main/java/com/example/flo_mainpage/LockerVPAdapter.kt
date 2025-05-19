8f1329f76cb482c8e20b08eff1d883cdabdaf265
heejung <mishel7@naver.com>
2025-05-19 22:50:35 +0900

    실습 8

--- FLO_MainPage/app/src/main/java/com/example/flo_mainpage/LockerVPAdapter.kt
+++ FLO_MainPage/app/src/main/java/com/example/flo_mainpage/LockerVPAdapter.kt
@@ -2,15 +2,15 @@ package com.example.flo_mainpage
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.flo_mainpage.databinding.FragmentLockerMusicfileBinding
 
-class LockerVPAdapter (fragment : Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int  = 2
+class LockerVPAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
-        return when(position){
-            0 -> SavedAlbumFragment()
-            else -> MusicFileFragment()
+        return when(position) {
+            0 -> SavedSongFragment()
+            1 -> MusicFileFragment()
+            else -> SavedAlbumFragment()
         }
     }
 }
